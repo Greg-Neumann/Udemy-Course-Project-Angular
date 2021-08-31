08e371b504910d8b26d6a789311f93afdebab815
Greg on his ChromeBook <greginthenorth@gmail.com>
2021-08-31 16:02:50 +0100

    Basic navigation menu bar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
-const MaterialComponents = [MatToolbarModule]; // Array of material components used in this app
+const MaterialComponents = [MatButtonModule, MatIconModule, MatToolbarModule]; // Array of material components used in this app
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents],
