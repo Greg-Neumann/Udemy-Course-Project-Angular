00af70c3b5172ea17ae632e2b521a5a734b79a89
Greg on his ChromeBook <greginthenorth@gmail.com>
2021-09-01 12:27:32 +0100

    Basic Material UI to lecture 51 inclusive

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
-const MaterialComponents = [MatButtonModule, MatIconModule, MatToolbarModule]; // Array of material components used in this app
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatGridListModule,
+  MatToolbarModule,
+]; // Array of material components used in this app
 @NgModule({
   imports: [MaterialComponents],
   exports: [MaterialComponents],
